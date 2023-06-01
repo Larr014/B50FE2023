f0ab440f25afb0f78c74ae71ed9f0f8a2dc12abe
Larr014 <larr014@gmail.com>
2023-06-01 17:11:49 -0400

    Clase 2 Editar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var registrar = ()=>{
+    let eContenedorTabla = document.getElementById("contenedorTabla");
+    let eSBtnAccion = document.getElementById("sBtnAccion");
     let eNombre = document.getElementById("nombre");
     let eApellido = document.getElementById("apellido");
     let nombre = eNombre.value;
@@ -15,6 +17,33 @@ var registrar = ()=>{
     }else{
         var listadoNuevo = [...listaAntiguo,persona]
     }
+    console.log(listadoNuevo)
     localStorage.setItem("alumnos",JSON.stringify(listadoNuevo));
+    render = "<table>"
+    render += "<tr><th>Nombre</th><th>Apellido</th><th>Accion</th></tr>"
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        const element = listadoNuevo[i];
+        render+="<tr>"
+        render+="<td>"+element.nombre+"</td>";
+        render+="<td>"+element.apellido+"</td>";
+        render+="<td>"
+        render+="<button id='btnEditar"+i+"'>Editar</button>"
+        render+="<button>Eliminar</button>"
+        render+="</td>"
+        render+="</tr>"
+    }
+    render+="</table>"
+    eContenedorTabla.innerHTML = render;
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        const element = listadoNuevo[i];
+        var eBtnEditar = document.getElementById("btnEditar"+i);
+        eBtnEditar.addEventListener("click",()=>{
+            let sBtn = "<button type='button' id='btnEditar' value='"+i+"'>Editar</button>"
+            eSBtnAccion.innerHTML = sBtn
+            eNombre.value = element.nombre;
+            eApellido.value = element.apellido;
+        })
+        
+    }
 }
 document.getElementById("btn").addEventListener("click",registrar)
